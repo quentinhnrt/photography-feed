a2675856a2979ec32321ac12e95c3bdd07dc53ee
Quentin Honnart <quentin.honnart@gmail.com>
2022-10-12 12:25:34 +0200

    init

--- src/helpers/InstagramApi.ts
+++
@@ -1,7 +0,0 @@
-export default class InstagramApi {
-    static async getPosts() {
-        const response = await fetch('https://www.instagram.com/username/?__a=1')
-        const data = await response.json()
-        return data
-    }
-}
